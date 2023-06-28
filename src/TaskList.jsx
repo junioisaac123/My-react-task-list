fffa37e55f56eb73ed5b98cd3dc384ba3cf8764f
junior <jisaac197@gmail.com>
2023-06-27 21:15:53 -0500

    validacion con formularios solo se vale tareas con mas de 3 carcteres

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -5,15 +5,24 @@ import useTaskManager from "./useTaskManager";
 function TaskList() {
   const { tasks, createTask, deleteTask, updateTask } = useTaskManager();
   const [newTaskName, setNewTaskName] = useState("");
+  const [newTaskDescription, setNewTaskDescription] = useState("");
 
+  const handleNewTaskDescription = (event) => {
+    setNewTaskDescription(event.target.value);
+  };
   const handleNewTaskNameChange = (event) => {
     setNewTaskName(event.target.value);
   };
 
-  const handleAddTask = () => {
-    const newTask = { name: newTaskName, completed: false };
-    createTask(newTask);
-    setNewTaskName("");
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if(newTaskName.length>3){
+      const newTask = { name: newTaskName,description: newTaskDescription, completed: false };
+      createTask(newTask);
+      setNewTaskName("");
+      setNewTaskDescription("");
+    }else{alert("El nombre de la tarea debe tener más de 3 caracteres.");}
+    
   };
 
   const handleTaskCompletion = (taskIndex) => {
@@ -32,24 +41,32 @@ function TaskList() {
 
   return (
     <div>
+      <form action="">
       <div>
         <input
           type="text"
           value={newTaskName}
           onChange={handleNewTaskNameChange}
         />
+        <input 
+          type="text"
+          value={newTaskDescription}
+          onChange={handleNewTaskDescription}
+           />
         <button onClick={handleAddTask}>Agregar</button>
       </div>
       {tasks.map((task, index) => (
         <Task
           key={index}
           name={task.name}
+          description={task.description}
           completed={task.completed}
           onCompletion={() => handleTaskCompletion(index)}
           onDelete={() => handleTaskDeletion(index)}
           onUpdate={(updatedTask) => handleTaskUpdate(index, updatedTask)}
         />
       ))}
+      </form>
     </div>
   );
 }
